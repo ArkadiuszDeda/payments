23035f686ea6f07b9eb6ea9829f30a79f82b8f4a
ArkadiuszDeda <arkadiusz.deda@gmail.com>
2021-01-17 20:17:28 +0000

    Xunit + some ground work for tests

---
+++ ClearBank.DeveloperTest.Tests/Services/PaymentServiceTests.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace ClearBank.DeveloperTest.Tests.Services
+{
+    public class PaymentServiceTests
+    {
+        [Fact]
+        public void MakePayment_AccountIsNull_ReturnsFalse()
+        {
+            Assert.True(true);
+        }
+    }
+}
