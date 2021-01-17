6692d1c287ec7d22ac1dff94e7435a2653e80117
ArkadiuszDeda <arkadiusz.deda@gmail.com>
2021-01-17 20:17:04 +0000

    DI added for AccountDataStore with example how it could be setup

---
+++ ClearBank.DeveloperTest/FakeExampleStartup.cs
@@ -0,0 +1,31 @@
+﻿using ClearBank.DeveloperTest.Data;
+using System.Configuration;
+
+namespace ClearBank.DeveloperTest
+{
+    // Just an example how Payment service could be created using dependency injection
+    // Another option is to use some kind of factory pattern and create Payment service with desired dependency there
+    public class FakeExampleStartup
+    {
+        public void Register()
+        {
+            var dataStoreType = ConfigurationManager.AppSettings["DataStoreType"];
+
+            IAccountDataStore accountDataStore;
+            if (dataStoreType == "Backup")
+            {
+                accountDataStore = new BackupAccountDataStore();
+            }
+            else
+            {
+                accountDataStore = new AccountDataStore();
+            }
+
+            // And then i.e. 
+            // serviceCollection.AddSingleton<IPaymentService>(new PaymentService(accountDataStore));
+            // This being just an example of home recruitment task I decided against adding actual DI packages etc here
+            // like there is no real data access in DataStore
+            // Also singleton is just an example
+        }
+    }
+}
